e8a3aa79173e66b5a1ed2bfa48a9209896f19153
DavidBao003 <1353406961@qq.com>
2024-03-11 17:45:08 +0800

    finish problem 2

--- ps0/src/rules/RulesOf6005.java
+++ ps0/src/rules/RulesOf6005.java
@@ -40,7 +40,7 @@ public class RulesOf6005 {
         
         // throw new RuntimeException("implement me!");
 
-        return true;
+        return writtenByYourself && !availableToOthers && writtenAsCourseWork && citingYourSource && implementationRequired;
     }
     
     /**
